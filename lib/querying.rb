47d45b88e9c4821a9f0d0b04db11bf82368b9684
Mohammed Noori <kmanoorib@gmail.com>
2018-08-28 22:35:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species)
+  "SELECT characters.species
   FROM characters
   ORDER BY COUNT(character.species) DESC
   LIMIT 1"
