38786d5cdb32be5ead10c19957204574bfe7ebae
Mohammed Noori <kmanoorib@gmail.com>
2018-08-29 00:01:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.name, COUNT(characters.species)
+  "SELECT series.title, COUNT(characters.species)
   FROM series
   JOIN "
 end
