7e4b92e49f77323f8fe7be7ee3c507e06f703ef2
Mohammed Noori <kmanoorib@gmail.com>
2018-08-29 01:21:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,5 +45,6 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   JOIN character_books.character_id = characters.id
   GROUP BY characters.name
-  ORDER BY COUNT"
+  ORDER BY COUNT(characters.name DESC
+  LIMIT 1"
 end
