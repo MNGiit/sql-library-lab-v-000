ddf76fd84bcb3400b46bc2cdcb6d132a21a3e333
Mohammed Noori <kmanoorib@gmail.com>
2018-08-29 01:16:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,7 @@ def select_series_title_with_most_human_characters
   JOIN character_books ON character_books.book_id = books.id 
   JOIN characters ON character_books.character_id = characters.id 
   WHERE characters.species = 'human'
-  GROUP BY 
+  GROUP BY series.title
   LIMIT 1"
 end
 
